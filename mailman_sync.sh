78379394f35ca5a899173871c4e501230af7d08a
Torrance <torrance@fuzion.co.nz>
2014-05-29 01:08:36 +0000

    Add ability to source members from multiple groups and merge into single mailman list.

--- mailman_sync.sh
+++ mailman_sync.sh
@@ -45,10 +45,18 @@ done
 if [ -n "$MAP" ] && [ -n "$KEY" ]; then
   curl $CURL_ARGS --fail --silent --show-error "$MAP?mailman_key=$KEY" | while read mapping; do
     set $mapping
+
+    # Check all arguments are present
+    if [ -z "$1" ] || [ -x "$2" ]; then
+      echo "Incorrect arguments supplied: $mapping"
+      echo "Bailing all further synchronizations."
+      exit 1
+    fi
+
     if [ "$VERBOSE" = 1 ]; then
       echo "Synchronizing group $1"
     fi
-    curl $CURL_ARGS --fail --silent --show-error "$2""?mailman_key=$KEY" | ifne sync_members $DRYRUN -f - $1
+    curl $CURL_ARGS --fail --silent --show-error "$2""?mailman_key=$KEY" | ifne /usr/lib/mailman/bin/sync_members $DRYRUN -f - $1
   done
 else
   usage
